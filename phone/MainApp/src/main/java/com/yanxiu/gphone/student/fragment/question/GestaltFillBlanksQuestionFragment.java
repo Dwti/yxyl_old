6cdd63f8b9888719d32e07aacf91702e2aeb322a
chenweiqi <chenweiqi@yanxiu.com>
2016-07-28 11:47:30 +0800

    完型填空

--- phone/MainApp/src/main/java/com/yanxiu/gphone/student/fragment/question/GestaltFillBlanksQuestionFragment.java
+++ phone/MainApp/src/main/java/com/yanxiu/gphone/student/fragment/question/GestaltFillBlanksQuestionFragment.java
@@ -1,15 +1,29 @@
 package com.yanxiu.gphone.student.fragment.question;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.common.core.utils.LogInfo;
+import com.yanxiu.gphone.student.R;
 import com.yanxiu.gphone.student.bean.AnswerBean;
 import com.yanxiu.gphone.student.view.question.QuestionsListener;
 
 /**
  * Created by Administrator on 2016/7/28.
  */
-public class GestaltFillBlanksFragment extends BaseQuestionFragment implements QuestionsListener, PageIndex{
+public class GestaltFillBlanksQuestionFragment extends BaseQuestionFragment implements QuestionsListener, PageIndex{
 
     private AnswerBean bean;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_gestaltfillblanks,null);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 
     @Override
     public int getPageIndex() {
