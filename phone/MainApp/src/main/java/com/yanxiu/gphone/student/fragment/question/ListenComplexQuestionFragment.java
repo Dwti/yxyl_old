abd5a86aa60a4607f727173e7f8c1b4f1d2b67e2
sunpeng <sunpeng@yanxiu.com>
2016-08-04 13:57:01 +0800

    add blank test

--- phone/MainApp/src/main/java/com/yanxiu/gphone/student/fragment/question/ListenComplexQuestionFragment.java
+++ phone/MainApp/src/main/java/com/yanxiu/gphone/student/fragment/question/ListenComplexQuestionFragment.java
@@ -209,6 +209,7 @@ public class ListenComplexQuestionFragment extends BaseQuestionFragment implemen
                 mSimplePlayer.setProgress(mediaPlayer.getCurrentPosition());
                 Log.i("progress", mediaPlayer.getCurrentPosition() + "");
                 // 每次延迟100毫秒再启动线程
+                
                 handler.postDelayed(updateThread, 100);
             }
         }
