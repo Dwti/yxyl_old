957f4db261f6d5edbe98ea3116f3eee54d9e7f8c
yangjj <yangjunjian@yanxiu.com>
2016-08-31 11:55:08 +0800

    Change complex question up and down picture

--- phone/MainApp/src/main/java/com/yanxiu/gphone/student/fragment/question/ClassfyQuestionFragment.java
+++ phone/MainApp/src/main/java/com/yanxiu/gphone/student/fragment/question/ClassfyQuestionFragment.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.common.core.view.UnMoveGridView;
 import com.yanxiu.gphone.student.R;
 import com.yanxiu.gphone.student.bean.AnswerBean;
 import com.yanxiu.gphone.student.view.question.QuestionsListener;
+import com.yanxiu.gphone.student.view.question.YXiuAnserTextView;
 
 /**
  * Created by Yangjj on 2016/8/30.
@@ -19,6 +21,9 @@ public class ClassfyQuestionFragment extends BaseQuestionFragment implements Que
 
     public int typeId;
 
+    private YXiuAnserTextView tvYanxiu;
+    private UnMoveGridView gvClassfyQuestion;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +32,23 @@ public class ClassfyQuestionFragment extends BaseQuestionFragment implements Que
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootView = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_classfy_question, null);
+        if (rootView == null) {
+            rootView = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_classfy_question, null);
+        }
+        initView();
+        initData();
         return rootView;
+    }
+
+    private void initView() {
+        tvYanxiu = (YXiuAnserTextView) rootView.findViewById(R.id.yxiu_tv);
+        gvClassfyQuestion = (UnMoveGridView) rootView.findViewById(R.id.classfy_question_item);
+    }
+
+    private void initData() {
+        if (questionsEntity != null && questionsEntity.getStem() != null) {
+            tvYanxiu.setTextHtml(questionsEntity.getStem());
+        }
     }
 
     @Override
