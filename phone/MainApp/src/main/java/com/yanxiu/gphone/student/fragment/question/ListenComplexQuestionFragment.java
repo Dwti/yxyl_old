d72ee7c03925e3d85b4cec2020deb9dbe13b369d
chenweiqi <chenweiqi@yanxiu.com>
2016-10-09 16:19:17 +0800

    imagebutton改为imageview

--- phone/MainApp/src/main/java/com/yanxiu/gphone/student/fragment/question/ListenComplexQuestionFragment.java
+++ phone/MainApp/src/main/java/com/yanxiu/gphone/student/fragment/question/ListenComplexQuestionFragment.java
@@ -20,6 +20,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
