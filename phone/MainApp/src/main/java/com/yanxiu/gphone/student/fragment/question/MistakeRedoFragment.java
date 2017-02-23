ffbd17b37e2d3d677a9c64be9842b1b4927431ee
chenweiqi <chenweiqi@yanxiu.com>
2017-02-23 10:21:19 +0800

    错题重做解析

--- phone/MainApp/src/main/java/com/yanxiu/gphone/student/fragment/question/MistakeRedoFragment.java
+++ phone/MainApp/src/main/java/com/yanxiu/gphone/student/fragment/question/MistakeRedoFragment.java
@@ -3,11 +3,17 @@ package com.yanxiu.gphone.student.fragment.question;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.yanxiu.gphone.student.R;
+import com.yanxiu.gphone.student.bean.ExtendEntity;
+import com.yanxiu.gphone.student.bean.QuestionEntity;
+import com.yanxiu.gphone.student.utils.YanXiuConstant;
+import com.yanxiu.gphone.student.view.question.YXiuAnserTextView;
+import com.yanxiu.gphone.student.view.question.subjective.SubjectiveStarLayout;
 
 /**
  * Created by Canghaixiao.
@@ -17,10 +23,53 @@ import com.yanxiu.gphone.student.R;
 
 public class MistakeRedoFragment extends Fragment {
 
+    private QuestionEntity questionsEntity;
+    private YXiuAnserTextView tvReportParseStatueText;
+    private SubjectiveStarLayout difficultyStart;
+    private YXiuAnserTextView tvReportParseText;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        questionsEntity = getArguments() != null ? (QuestionEntity) getArguments().getSerializable("questions") : null;
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_mistakeredo,container,false);
+        View view = inflater.inflate(R.layout.fragment_mistakeredo, container, false);
+        initView(view);
+        initData();
         return view;
+    }
+
+    private void initData() {
+        if (questionsEntity==null){
+            return;
+        }
+        if (questionsEntity.getExtend() != null && questionsEntity.getExtend().getData() != null) {
+            ExtendEntity.DataEntity dataEntity = questionsEntity.getExtend().getData();
+            switch (questionsEntity.getTemplate()) {
+                case YanXiuConstant.CLASSIFY_QUESTION://归类
+                    tvReportParseStatueText.setClasfyFlag(false);
+                    tvReportParseStatueText.setTextHtml(dataEntity.getAnswerCompare().replaceAll("<img", "<imgFy"));
+                    break;
+                default:
+                    tvReportParseStatueText.setTextHtml(dataEntity.getAnswerCompare());
+
+            }
+        }
+        difficultyStart.selectStarCount(questionsEntity.getDifficulty());
+
+        if (!TextUtils.isEmpty(questionsEntity.getAnalysis())) {
+            tvReportParseText.setTextHtml(questionsEntity.getAnalysis());
+        }
+    }
+
+    private void initView(View rootView) {
+        tvReportParseStatueText = (YXiuAnserTextView) rootView.findViewById(R.id.hw_report_parse_statue_text);
+        difficultyStart = (SubjectiveStarLayout) rootView.findViewById(R.id.view_sub_difficulty_star);
+        tvReportParseText = (YXiuAnserTextView) rootView.findViewById(R.id.hw_report_parse_text);
+
     }
 }
