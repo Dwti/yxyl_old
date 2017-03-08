f9d03affcb8d3d167cfc60e9d94c46d29284cc16
chenweiqi <chenweiqi@yanxiu.com>
2017-03-08 10:34:30 +0800

    根据接口文档新增javabean

---
+++ phone/MainApp/src/main/java/com/yanxiu/gphone/student/bean/MistakeRedoNumberBean.java
@@ -0,0 +1,44 @@
+package com.yanxiu.gphone.student.bean;
+
+import com.yanxiu.basecore.bean.YanxiuBaseBean;
+
+/**
+ * Created by Canghaixiao.
+ * Time : 2017/3/7 11:25.
+ * Function :
+ */
+
+public class MistakeRedoNumberBean implements YanxiuBaseBean {
+
+    private DataStatusEntityBean status;
+
+    private ProPerty property;
+
+    public class ProPerty{
+        private int questionNum;
+
+        public int getQuestionNum() {
+            return questionNum;
+        }
+
+        public void setQuestionNum(int questionNum) {
+            this.questionNum = questionNum;
+        }
+    }
+
+    public DataStatusEntityBean getStatus() {
+        return status;
+    }
+
+    public void setStatus(DataStatusEntityBean status) {
+        this.status = status;
+    }
+
+    public ProPerty getProperty() {
+        return property;
+    }
+
+    public void setProperty(ProPerty property) {
+        this.property = property;
+    }
+}
