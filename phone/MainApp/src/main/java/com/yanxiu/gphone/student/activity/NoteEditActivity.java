b0fdc82dd3d858cb89ea9275248d56c62b847c98
sunpeng <sunpeng@yanxiu.com>
2017-03-20 18:01:40 +0800

    fix bug mes-1179

--- phone/MainApp/src/main/java/com/yanxiu/gphone/student/activity/NoteEditActivity.java
+++ phone/MainApp/src/main/java/com/yanxiu/gphone/student/activity/NoteEditActivity.java
@@ -214,6 +214,8 @@ public class NoteEditActivity extends Activity implements View.OnClickListener {
             if (response.getStatus().getCode() == 0) {
                 NoteRequest noteRequest = (NoteRequest) request;
                 setResultOK(noteRequest.getNote().getImages(), noteRequest.getNote().getText());
+            }else {
+                ToastMaster.showShortToast(NoteEditActivity.this, response.getStatus().getDesc());
             }
         }
 
