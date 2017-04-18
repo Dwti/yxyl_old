cfe736a56d0c77b52be5089dbd4a7746eff0191d
sunpeng <sunpeng@yanxiu.com>
2017-04-18 16:16:23 +0800

    归类题html常用标签支持

--- phone/MainApp/src/main/java/com/yanxiu/gphone/student/view/question/classfy/ClassfyDelPopupWindow.java
+++ phone/MainApp/src/main/java/com/yanxiu/gphone/student/view/question/classfy/ClassfyDelPopupWindow.java
@@ -3,6 +3,7 @@ package com.yanxiu.gphone.student.view.question.classfy;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.text.Html;
 import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -116,7 +117,8 @@ public class ClassfyDelPopupWindow extends BasePopupWindow  {
                         LayoutInflater inflater = LayoutInflater.from(mContext);
                         final View containerView = inflater.inflate(R.layout.classfy_layout_textview_image, null);
                         TextView classfy_answer_popup_text = (TextView) containerView.findViewById(R.id.classfy_answer_popup_text);
-                        Spanned spanned = ClassfyAnswers.fromHtml(mContext,classfyPopItem.get(i).getName());
+//                        Spanned spanned = ClassfyAnswers.fromHtml(mContext,classfyPopItem.get(i).getName());
+                        Spanned spanned = Html.fromHtml(classfyPopItem.get(i).getName());
                         classfy_answer_popup_text.setText(spanned);
                         //view.setText(mQuestionsEntity.getContent().getChoices().get(i));
 
